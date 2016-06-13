6fe9de6325a593a3bc9af93d69dfd1e359148424
MNB <buyskih@gmail.com>
2016-06-13 18:00:18 +0300

    Tests for jquery fit width

--- source/FitWidth.ts
+++ source/FitWidth.ts
@@ -10,10 +10,10 @@ module Datetime {
             this.$element = $(this.element);
             this.$element.width(spanWidth(this.$element, this.length) + correction);
         }
-
     }
+
     export function spanWidth(sourceInput:JQuery, length:number) {
-        let span = $('<span>' + StrRepeat('0', length) + '</span>');
+        let span = $(`<span>${StrRepeat('0', length)}</span>`);
         span.css(sourceInput.css(['font', 'padding', 'line-height']));
         $('body').append(span);
         let width = span.outerWidth();
--- tests/FitWidth.spec.ts
+++ tests/FitWidth.spec.ts
@@ -1,7 +1,11 @@
 describe('FitWidth', () => {
     it('should return correct width for symbols', () => {
         let el = $('<input style="font-family: monospace; font-size: 10px; padding: 0px">');
-
         expect(Datetime.spanWidth(el, 2) / Datetime.spanWidth(el, 1)).toBe(2);
+    });
+    it('should set input width', () => {
+        let el = $('<input style="font-family: monospace; font-size: 10px; padding: 0px">');
+        new Datetime.FitWidth(el, 2);
+        expect(el.width()).toBe(Datetime.spanWidth(el, 2));
     });
 });
