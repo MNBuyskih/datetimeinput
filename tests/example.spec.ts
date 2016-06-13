06853a432a0a7e7b342ac8d9cf078c67d13b8cbf
MNB <buyskih@gmail.com>
2016-06-13 16:32:42 +0300

    setup karma

---
+++ tests/example.spec.ts
@@ -0,0 +1,8 @@
+describe('test', () => {
+    it('should be ok', () => {
+        expect(true).toBe(true);
+    });
+    it('should be false', () => {
+        expect(false).toBe(false);
+    });
+});
